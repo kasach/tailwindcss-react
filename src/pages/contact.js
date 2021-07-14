5c73fe212fc014247e58d290b34af1dc496a13c0
kasach <eddi259@live.de>
2021-07-14 16:41:05 +0200

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="h-screen flex justify-center items-center bg-blue-300">
-      <h1 className="text-9xl uppercase font-black">Contact Page</h1>
+      <h1 className="text-9xl uppercase font-black">Contact sPage</h1>
     </div>
   );
 };
